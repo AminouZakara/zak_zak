1cb1cfd3999467d1f4a359b62f8a9bce6c29ff1c
AminouZakara <aminouzakara@gmail.com>
2023-06-02 17:03:50 +0300

    developed by Dev_2

---
+++ My_1App/boom.js
@@ -0,0 +1,4 @@
+Hi I'm an JS file from Dev_2
+Hi I'm an JS file from Dev_2
+Hi I'm an JS file from Dev_2
+
---
+++ My_1App/boom.py
@@ -0,0 +1,3 @@
+Hi I'm an PY file from Dev_2
+Hi I'm an PY file from Dev_2
+Hi I'm an PY file from Dev_2
